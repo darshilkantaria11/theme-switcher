e159b75aa87857b4be1a42cf63906572c374cb77
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-01-24 13:33:19 +0530

    completed

---
+++ app/components/Theme.js
@@ -0,0 +1,29 @@
+"use client"
+// themes.js
+export const lightTheme = {
+  primary: '#ffffff',
+  secondary: '#f0f0f0',
+  accent: '#3498db',
+  // Add other theme colors as needed
+};
+
+export const darkTheme = {
+  primary: '#333333',
+  secondary: '#1a1a1a',
+  accent: '#ffcc00',
+  // Add other theme colors as needed
+};
+
+export const forestTheme = {
+  primary: '#006400',
+  secondary: '#228b22',
+  accent: '#ffd700',
+  // Add other theme colors as needed
+};
+
+export const fireTheme = {
+  primary: '#8b0000',
+  secondary: '#ff4500',
+  accent: '#ffd700',
+  // Add other theme colors as needed
+};
